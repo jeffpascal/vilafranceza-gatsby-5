efa552323f534513db5f36ca23550f289225e868
Pascal Jean-Francois <jeffpascal96@gmail.com>
2023-02-04 16:09:42 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,77 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `vilafranceza-gatsby-5`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://blog.vilafranceza.ro/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  },{       
+    resolve: '@pasdo501/gatsby-source-woocommerce',
+    options: {
+      // Base URL of Wordpress site
+      api: 'blog.vilafranceza.ro',
+
+      // set to false to not see verbose output during build 
+      // default: true
+      verbose: true,
+
+      // true if using https. otherwise false.
+      https: true,
+      api_keys: {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+      },
+      // Array of strings with fields you'd like to create nodes for...
+      fields: ['products', 'products/categories', 'products/attributes'],
+      // Send the API keys as query string parameters instead of using the authorization header
+      // OPTIONAL: defaults to false
+      query_string_auth: false,
+      // Version of the woocommerce API to use
+      // OPTIONAL: defaults to 'wc/v3'
+      api_version: 'wc/v3',
+      // OPTIONAL: How many results to retrieve *per request*
+      per_page: 100,
+      // OPTIONAL: Custom WP REST API url prefix, only needed if not using 
+      // the default prefix ('wp-json').
+      // wpAPIPrefix: 'wp-json',
+      // OPTIONAL: Support for URLs with ports, e.g. 8080; defaults to no port
+      // port: '8080',
+      // OPTIONAL: Encoding; default to 'utf8'
+      encoding: 'utf8',
+      // OPTIONAL: Custom Axios config (see https://github.com/axios/axios) - note that this can override other options.
+      axios_config: {
+        // Axios config options
+      }
+    }
+  }]
+};
